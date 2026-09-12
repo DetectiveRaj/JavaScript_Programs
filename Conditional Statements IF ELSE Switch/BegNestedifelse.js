ea0f21209cb7afc18862c458b51b33570d6dfa6d
vishalrajkit <vishalrajmailbox@gmail.com>
2026-09-12 13:56:56 +0530

    Commit for Nested if else file

---
+++ Conditional Statements IF ELSE Switch/BegNestedifelse.js
@@ -0,0 +1,14 @@
+let marks = 50;
+
+if (marks >= 90) {
+    console.log("Grade is A++");
+}else if (marks >= 80) {
+    console.log("grade is A");
+}else if(marks >= 70) {
+    console.log("Grade is B");
+}else if (marks >= 60) {
+    console.log("Grade is 60");
+}else {
+    console.log("Grade is Fail");
+}
+
