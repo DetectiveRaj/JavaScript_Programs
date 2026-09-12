909811f18bbb690e8840ba136bd0828a1c37446a
vishalrajkit <vishalrajmailbox@gmail.com>
2026-09-12 15:57:18 +0530

    commit for login validation file

---
+++ Conditional Statements IF ELSE Switch/Login_Valid_ifelse.js
@@ -0,0 +1,11 @@
+let username = "admin";
+let password = "admin123";
+
+if (username === "admin" && password === "admim123") {
+    console.log("Login Sucesssfull");
+
+}
+else {
+
+console.log("Login fail");
+}
