d9beab2469ccbc75e1ceb19126bc3785c0ff2fbd
vishalrajkit <vishalrajmailbox@gmail.com>
2026-09-12 17:34:10 +0530

    Commit for string value in switch case code file

--- Conditional Statements IF ELSE Switch/BegSwitchCase.js
+++ Conditional Statements IF ELSE Switch/BegSwitchCase.js
@@ -1,27 +1,54 @@
-let day = "Sat";
+/*
+Switch case prog for number value of day to print the name of the day
+
+let day = 9;
 
 switch (day) {
 
-    case "Mon": console.log("Monday");
+    case 1: console.log("Monday");
     break;
 
-    case "Tue": console.log("Tuesday");
+    case 2: console.log("Tuesday");
     break;
 
-    case "Wed": console.log("Wednesday");
+    case 3: console.log("Wednesday");
     break;
 
-    case "Thu": console.log("Thursday");
+    case 4: console.log("Thursday");
     break;
 
-    case "Fri": console.log("Friday");
+    case 5: console.log("Friday");
     break;
 
-    case "Sat": console.log("Saturday");
+    case 6: console.log("Saturday");
     break;
 
-    case "Sun": console.log("Sunday");
+    case 7: console.log("Sunday");
     break;
 
     default: console.log("not a right number");
+}
+
+*/
+
+//Switch with string
+
+let PaymentType = "gffgh"
+
+switch (PaymentType) {
+
+    case "UPI": console.log("UPI Payment");
+    break;
+
+    case "Card": console.log("Card Payment");
+    break;
+
+    case "NetBanking": console.log("NetBanking Payment");
+    break;
+
+    case "Cash": console.log("Cash Payment");
+    break;
+
+    default : console.log("Invalid Payment Type");
+
 }
