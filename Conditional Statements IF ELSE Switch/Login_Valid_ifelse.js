0d51ab1d35aacfcd81f2f453be74c485cf39cca6
vishalrajkit <vishalrajmailbox@gmail.com>
2026-09-12 16:14:38 +0530

    commit for modification done in code

--- Conditional Statements IF ELSE Switch/Login_Valid_ifelse.js
+++ Conditional Statements IF ELSE Switch/Login_Valid_ifelse.js
@@ -1,7 +1,9 @@
 let username = "admin";
 let password = "admin123";
+//adding boolean condition as well
+let accountactive = true;
 
-if (username === "admin" && password === "admim123") {
+if (username === "admin" && password === "admin123" && accountactive === true) {
     console.log("Login Sucesssfull");
 
 }
