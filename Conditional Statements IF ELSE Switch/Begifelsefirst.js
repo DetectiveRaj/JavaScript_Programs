5df062a3a67544a6bf8735ab5ee759deca5115e7
vishalrajkit <vishalrajmailbox@gmail.com>
2026-09-12 13:43:41 +0530

    commit for Conditional statment folder files

---
+++ Conditional Statements IF ELSE Switch/Begifelsefirst.js
@@ -0,0 +1,37 @@
+//Check age
+
+
+/*let age = 20;
+if (age >= 18){
+    console.log("You are eligible to vote");
+} 
+
+//if with string
+
+    let username = "admin";
+
+    if (username === "admin"){
+        console.log("You are logged in as admin");
+    } 
+
+    // Check whether a number is positive
+
+    let number = -5;
+    if(number > 0) {
+        console.log("number is positive");
+    }
+    else{
+        console.log("number is negative");
+    }
+        */
+
+    // Even or odd number
+
+    let num = 11;
+
+    if (num % 2 === 0) {
+        console.log("number is even");
+
+    } else {
+        console.log("number is odd");
+    }
